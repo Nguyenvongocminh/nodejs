b145b551656f55fdfdfc17994d3616961206d491
Ngoc <minh01294716049@gmail.com>
2022-10-21 22:28:17 +0700

    ngay 2

--- controllers/products.js
+++ controllers/products.js
@@ -1,34 +1,25 @@
 const Products = require("../models/products");
 
-//CRUD
-//CREATE-POST
+// CRUD
+// CREATE - POST
 const createProduct = (req, res, next) => {
   try {
-    const {
-      productName,
-      productBrand,
-      type,
-      price,
-      discount,
-      quantity,
-      images,
-    } = req.body;
+    const { productName, productBrand, type, price, quantity, images } =
+      req.body;
     if (
       !productName ||
       !productBrand ||
       !type ||
       !price ||
-      !discount ||
       !quantity ||
       !images
     ) {
       res.status(400).json({
-        statuscode: 400,
-        message: "Some fileds are not empty.",
+        statusCode: 400,
+        message: "Some fields are not empty.",
       });
     }
     let product = new Products(req.body);
-    // product.save()
     product.save().then((response) => {
       res.json({
         message: "Added product successfully!",
@@ -39,7 +30,138 @@ const createProduct = (req, res, next) => {
   }
 };
 
-//READ-GET  || POST
-//UPDATE-PUT||PATCH
-//DELETE-DELETE
-module.exports = { createProduct };
+//get all products
+
+const getAllProducts = async (req, res, next) => {
+  try {
+    const allProducts = await Products.find();
+    if (allProducts.length > 0) {
+      res.status(200).json({
+        product: allProducts.reverse(),
+      });
+    } else {
+      res.status(200).json({
+        massage: "No results",
+        products,
+      });
+    }
+  } catch (error) {
+    console.log("erorr", erorr);
+    res.status(400).json({
+      message: "Bad request",
+    });
+  }
+};
+
+//get by id
+const getProductById = async (req, res, next) => {
+  const productId = req.params.productId;
+  try {
+    const product = await Products.findById(productId);
+    if (product) {
+      res.status(200).json({
+        statusCode: 200,
+        product,
+      });
+    } else {
+      res.json({
+        statusCode: 204,
+        massage: "This  product Id have not in the database",
+        products: {},
+      });
+    }
+  } catch (error) {
+    console.log("erorr", erorr);
+    res.status(400).json({
+      message: "Bad request",
+    });
+  }
+};
+
+//delete product by id
+const deleteProductById = async (req, res, next) => {
+  const productId = req.params.productId;
+  try {
+    const product = await Products.findByIdAndRemove(productId);
+    if (product) {
+      res.status(200).json({
+        statusCode: 200,
+        massage: "Delete product successfully",
+      });
+    } else {
+      res.json({
+        statusCode: 204,
+        massage: "This  product Id have not in the database",
+      });
+    }
+  } catch (error) {
+    console.log("erorr", erorr);
+    res.status(400).json({
+      message: "Bad request",
+    });
+  }
+};
+
+const editProduct = (req, res, next) => {
+  try {
+    let productId = req.params.productId;
+    if (!req.body) {
+      return res.send({
+        statuscode: 404,
+        message: "Body request can not emty.",
+      });
+    }
+    Products.findByIdAndUpdate(productId, req.body).then((data) => {
+      if (data) {
+        res.status(200).json({
+          statuscode: 200,
+          message: "Update product successfully",
+        });
+      } else {
+        res.json({
+          statuscode: 204,
+          message: "This product Id is have not in the database ",
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      statuscode: 400,
+      message: "Bad request",
+    });
+  }
+};
+
+const addProduct = async (req, res, next) => {
+  try {
+    // const { name, brand, price, type, quantity } = req.body;
+    // if (!name || !brand || !type || !price || !quantity) {
+    //   res.status(400).json({ message: 'Some fields not null' });
+    // }
+    const result = await addProductSchema.validateAsync(req.body);
+    // let product = new Products(req.body);
+    let product = new Products(result);
+    product.save().then((response) => {
+      res.json({
+        message: "Added product Successfully!",
+      });
+    });
+  } catch (error) {
+    console.log("ERRORS: ", error);
+    return res.status(400).json({
+      errors: error.details,
+    });
+  }
+};
+
+// READ - GET || POST
+// UPDATE - PUT || PATCH
+// DELETE - DELETE
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  deleteProductById,
+  editProduct,
+};
--- routes/products.js
+++ routes/products.js
@@ -1,35 +1,36 @@
-const express=require('express')
-const route=express.Router()
+const express = require("express");
+const cors = require("cors");
+const route = express.Router();
+const app = express();
 
-const app=express()
-const cors=require('cors')
 //CORS middleware
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH')
-    res.header('Access-Control-Allow-Headers', 'Content-Type')
-  
-    next()
-  }
-  app.use(allowCrossDomain)
-const ProductsController=require('..//controllers/products')
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
 
+  next();
+};
 
-app.use(cors({ origin: '*', credentials: true }))
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  )
-  next()
-})
+const ProductsController = require("../controllers/products");
 
 // app.use(cors({ origin: '*', credentials: true }))
+app.use(allowCrossDomain);
 
+route.post("/api/products/create", ProductsController.createProduct);
+route.get("/api/products/getAllProducts", ProductsController.getAllProducts);
+route.get(
+  "/api/products/getProductById/:productId",
+  ProductsController.getProductById
+);
+route.delete(
+  "/api/products/deleteProductById/:productId",
+  ProductsController.deleteProductById
+);
 
-route.post('/api/products/create',ProductsController.createProduct)
+route.patch(
+  "/api/products/editProductById/:productId",
+  ProductsController.editProduct
+);
 
-// prefix
-
-module.exports=route
+module.exports = route;
