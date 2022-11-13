8471ac9b013f778df3a57c66cb60b67836882c26
Ngoc <minh01294716049@gmail.com>
2022-11-13 17:13:10 +0700

    update getallproduct

--- controllers/products.js
+++ controllers/products.js
@@ -27,21 +27,122 @@ const createProduct = async (req, res, next) => {
 
 //get all products
 
+// const getAllProducts = async (req, res, next) => {
+//   try {
+//     const {
+//       pageSize = 12,
+//       pageNumber = 1,
+//       productName = "",
+//       productBrand = "",
+//       orderByColumn,
+//       orderByDirection = "desc",
+//     } = req.query;
+
+//     const filter = {
+//       $and: [
+//         {
+//           productName: {
+//             $regex: productName,
+//             $options: "$i",
+//           },
+//         },
+//         {
+//           productBrand: {
+//             $regex: productBrand,
+//             $options: "$i",
+//           },
+//         },
+//       ],
+//     };
+
+//     const filterProducts = await Products.find(filter)
+//       .sort(`${orderByDirection === "asc" ? "" : "-"}${orderByColumn}`)
+//       .limit(pageSize * 1)
+//       .skip((pageNumber - 1) * pageSize);
+//     console.log(orderByDirection);
+//     console.log(orderByColumn);
+
+//     const allProducts = await Products.find(filter);
+
+//     let totalPage = Math.ceil(allProducts.length / pageSize);
+//     if (filterProducts.length > 0) {
+//       res.status(200).json({
+//         totalPage,
+//         totalProducts: allProducts.length,
+//         products:
+//           orderByColumn && orderByDirection
+//             ? filterProducts
+//             : filterProducts.reverse(),
+//       });
+//     } else {
+//       res.status(200).json({
+//         message: "No results",
+//         products: [],
+//       });
+//     }
+//   } catch (error) {
+//     res.status(400).json({
+//       message: "Bad request",
+//     });
+//   }
+// };
 const getAllProducts = async (req, res, next) => {
   try {
-    const allProducts = await Products.find();
+    const {
+      pageSize = 12,
+      pageNumber = 1,
+      productName = "",
+      productBrand = "",
+      orderByColumn,
+      orderByDirection = "desc",
+    } = req.query;
+    const filter = {
+      $and: [
+        {
+          productName: {
+            $regex: productName,
+            $options: "$i",
+          },
+        },
+        {
+          productBrand: {
+            $regex: productBrand,
+            $options: "$i",
+          },
+        },
+      ],
+    };
+    const filterProducts = await Products.find(filter)
+      .sort(`${orderByDirection === "asc" ? "" : "-"}${orderByColumn}`)
+      .limit(pageSize * 1)
+      .skip((pageNumber - 1) * pageSize);
+
+    const allProducts = await Products.find(filter);
+
+    let totalPage = 0;
+    if (allProducts.length % pageSize === 0) {
+      totalPage = allProducts.length / pageSize;
+    } else {
+      totalPage = parseInt(allProducts.length / pageSize) + 1;
+    }
+
     if (allProducts.length > 0) {
       res.status(200).json({
-        product: allProducts.reverse(),
+        totalPage: totalPage,
+        totalProducts: allProducts.length,
+        products:
+          orderByDirection && orderByColumn
+            ? filterProducts
+            : filterProducts.reverse(),
       });
     } else {
       res.status(200).json({
-        massage: "No results",
-        products,
+        message: "No results",
+        products: [],
       });
     }
   } catch (error) {
-    console.log("error", error);
+    console.log("error: ", error);
     res.status(400).json({
       message: "Bad request",
     });
